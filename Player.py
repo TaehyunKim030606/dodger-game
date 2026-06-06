431605647e4c6b7ff79779f2450b8383d6f35c1b
seul0103 <jys0103@gnu.ac.kr>
2026-06-06 23:45:59 +0900

    return 개수 바꿈
    Refactor Player class to include small player state and adjust image handling.

--- Player.py
+++ Player.py
@@ -3,16 +3,22 @@ import pygame
 import pygwidgets
 from Constants import *
 
+
 class Player():
     def __init__(self, window):
         self.window = window
         self.loc = (-100, -100)
+
         self.normalSurface = pygame.image.load('images/player.png').convert_alpha()
         normalRect = self.normalSurface.get_rect()
+
         smallSize = (normalRect.width // 2, normalRect.height // 2)
         self.smallSurface = pygame.transform.scale(self.normalSurface, smallSize)
+
         self.isSmall = False
+
         self.image = pygwidgets.Image(window, self.loc, self.normalSurface)
+
         playerRect = self.image.getRect()
         self.maxX = WINDOW_WIDTH - playerRect.width
         self.maxY = GAME_HEIGHT - playerRect.height
@@ -22,14 +28,17 @@ class Player():
             return
 
         self.isSmall = shouldBeSmall
+
         playerRect = self.image.getRect()
         self.loc = playerRect.topleft
+
         if self.isSmall:
             surface = self.smallSurface
         else:
             surface = self.normalSurface
 
         self.image = pygwidgets.Image(self.window, self.loc, surface)
+
         playerRect = self.image.getRect()
         self.maxX = WINDOW_WIDTH - playerRect.width
         self.maxY = GAME_HEIGHT - playerRect.height
@@ -41,6 +50,7 @@ class Player():
             x = 0
         elif x > self.maxX:
             x = self.maxX
+
         if y < 0:
             y = 0
         elif y > self.maxY:
@@ -48,11 +58,23 @@ class Player():
 
         self.loc = (x, y)
         self.image.setLoc(self.loc)
-        return self.image.getRect()
+
+        playerRect = self.image.getRect()
+
+        player_cx = playerRect.centerx
+        player_cy = playerRect.centery
+        player_radius = playerRect.width // 2
+
+        return player_cx, player_cy, player_radius
 
     def draw(self, isInvincible=False):
         self.image.draw()
+
         if isInvincible:
             playerRect = self.image.getRect()
-            pygame.draw.rect(self.window, (255, 255, 0),
-                             playerRect.inflate(8, 8), 3)
+            pygame.draw.rect(
+                self.window,
+                (255, 255, 0),
+                playerRect.inflate(8, 8),
+                3
+            )
